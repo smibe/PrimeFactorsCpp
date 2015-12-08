3282cf6b0bc3a17b4d52af9f43aab8707c315ba2
smibe <geh@geherbert.de>
2015-12-08 14:49:32 +0100

    Added first test

--- PrimeFactors_UnitTest/PrimeFactors_UnitTest.cpp
+++ PrimeFactors_UnitTest/PrimeFactors_UnitTest.cpp
@@ -2,10 +2,23 @@
 #include "TestUtilities.h"
 #include <vector>
 
+class PrimeFactors
+{
+public:
+    static IntVector Generate(int number)
+    {
+        throw std::invalid_argument("Number needs to be greater 1");
+    }
+};
+
 namespace PrimeFactors_UnitTest
 {
     TEST_CLASS(PrimeFactorsTest)
     {
     public:
+        TEST_METHOD(CalculatePrimeOf_1)
+        {
+            Assert::ExpectException<std::exception>([]() { PrimeFactors::Generate(1); });
+        }
     };
 }
